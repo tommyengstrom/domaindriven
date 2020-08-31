c958b125d705f8867086671418b1343e9d343f31
Tommy Engström <tommy@tommyengstrom.com>
2020-08-31 16:56:06 +0200

    Changes that I forgot to commit earlier

--- src/DomainDriven/Persistance/Postgres.hs
+++ src/DomainDriven/Persistance/Postgres.hs
@@ -6,7 +6,9 @@ import           RIO.Time
 import           Database.PostgreSQL.Simple
 
 
-data PersistanceError = EncodingError String
+data PersistanceError
+    = EncodingError String
+    | ValueError String
     deriving (Show, Eq, Typeable, Exception)
 
 
@@ -16,14 +18,39 @@ data PostgresStateAndEvent model event = PostgresStateAndEvent
     { getConnection    :: IO Connection
     , queryEvents      :: Query
     , queryEventsAfter :: UTCTime -> Query
-    , stateQuery       :: String
-    , stateWrite       :: String
+    , queryState       :: Query
+    , writeState       :: Query -- ^ Insert to write the state
+    , writeEvents      :: Query -- ^ Insert to write an event
     }
     deriving Generic
 
 
-instance PersistanceHandler (PostgresStateAndEvent model event) model event where
-    getModel _ = undefined
-    getEvents _ = undefined
-    transactionalUpdate _pg _appEvent _evalCmd = do
-        undefined
+instance (FromRow model, FromRow event, ToRow model, ToRow (Stored event))
+        => PersistanceHandler (PostgresStateAndEvent model event) model event where
+    -- | getModel will always return the latest model
+    getModel pg = do
+        conn <- getConnection pg
+        r    <- query_ conn (queryState pg)
+        case r of
+            x : _ -> pure x
+            _     -> throwM $ ValueError "Found no state"
+            -- ^ FIXME: I should recalculate the state, right?
+            -- I need the inital event and the
+
+    getEvents pg = do
+        conn <- getConnection pg
+        query_ conn (queryEvents pg)
+
+    transactionalUpdate pg appEvent evalCmd = do
+        conn <- getConnection pg
+        withTransaction conn $ do
+            m <- getModel pg
+            case evalCmd m of
+                Left  err        -> throwM err
+                Right (ret, evs) -> do
+                    storedEvs <- traverse toStored evs
+                    let newM = foldl' appEvent m storedEvs
+                        -- FIXME: Ensure the events are applied in the correct order!
+                    _ <- traverse (query conn (writeEvents pg)) storedEvs :: IO [[event]]
+                    _ <- query conn (writeState pg) newM :: IO [model]
+                    pure ret
