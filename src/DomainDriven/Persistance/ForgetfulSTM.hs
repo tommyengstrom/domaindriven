8d1f428969486c1c6856997828bb7ed37dd15ced
Tommy Engström <tommy@succinct.se>
2020-09-17 17:19:55 +0200

    rewrite forgetful model

--- src/DomainDriven/Persistance/ForgetfulSTM.hs
+++ src/DomainDriven/Persistance/ForgetfulSTM.hs
@@ -5,14 +5,24 @@ import           RIO
 import           GHC.IO.Unsafe                  ( unsafePerformIO )
 
 
-data PersistanceError = EncodingError String
+data ForgetfulError = ForgetfulError Text
     deriving (Show, Eq, Typeable, Exception)
+
+createForgetfulSTM
+    :: (model -> Stored event -> model)
+    -> model -- ^ initial model
+    -> IO (ForgetfulSTM model event)
+createForgetfulSTM appEvent m0 = do
+    state <- newTVarIO m0
+    evs <- newTVarIO []
+    pure $ ForgetfulSTM state appEvent m0 evs
 
 -- | STM state without event persistance
 data ForgetfulSTM model event = ForgetfulSTM
     { stateTVar :: TVar model
     , app       :: model -> Stored event -> model
     , seed      :: model
+    , events    :: TVar [Stored event]
     }
     deriving Generic
 
@@ -21,27 +31,21 @@ instance ReadModel (ForgetfulSTM m e) where
     type Event (ForgetfulSTM m e) = e
     applyEvent'  ff = app ff
     getModel' ff = readTVarIO $ stateTVar ff
-------------------------------------------------------------------------------------------
--- The old stuff
-------------------------------------------------------------------------------------------
-createForgetfulSTM
-    :: (model -> Stored event -> model)
-    -> model -- ^ initial model
-    -> IO (DomainModel (ForgetfulSTM model event) model event)
-createForgetfulSTM appEvent m0 = do
-    tvar <- newTVarIO m0
-    pure $ DomainModel (ForgetfulSTM tvar) appEvent
+    getEvents' ff = readTVarIO $ events ff
 
-instance PersistanceHandler (ForgetfulSTM model event) model event where
-    getModel (ForgetfulSTM tvar) = readTVarIO tvar
-    getEvents _ = pure [] -- ^ Events are not persisted
-    transactionalUpdate (ForgetfulSTM tvar) appEvent evalCmd = do
+instance WriteModel (ForgetfulSTM m e) where
+    type Error (ForgetfulSTM m e) = ForgetfulError
+    transactionalUpdate' ff evalCmd =
         atomically $ do
+            let tvar = stateTVar ff
             m         <- readTVar tvar
             (r, evs)  <- either throwM pure $ evalCmd m
             storedEvs <- for evs $ \e -> do
                 let s = unsafePerformIO $ toStored e
                 pure s
-            let newModel = foldl' appEvent m storedEvs
+            let newModel = foldl' (app ff) m storedEvs
+            modifyTVar (events ff) (<> storedEvs)
             writeTVar tvar newModel
+
             pure r
+
