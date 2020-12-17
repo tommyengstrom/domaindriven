1ea5aef56ffb195db1afc5749324b0fcf60ffb2d
Tommy Engström <tommy@succinct.se>
2020-12-17 17:54:04 +0100

    Support multiple field with same type

--- src/DomainDriven/Internal/WithNamedField.hs
+++ src/DomainDriven/Internal/WithNamedField.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module DomainDriven.Internal.WithNamedField where
 
@@ -12,38 +13,54 @@ import           Data.Text                      ( Text )
 import qualified Data.Text                                    as T
 import qualified Data.HashMap.Strict                          as HM
 import           Control.Applicative
+import           Control.Monad.State
+
+newtype WithNamedField a = WithNamedField a
+
+instance (GNamedToJSON (Rep a), Generic a) => ToJSON (WithNamedField a) where
+    toJSON (WithNamedField a) = gToNamedJson a
+
+instance (GNamedFromJSON (Rep a), Generic a) => FromJSON (WithNamedField a) where
+    parseJSON = fmap WithNamedField . gParseNamedJson
 
 gToNamedJson :: (GNamedToJSON (Rep a), Generic a) => a -> Value
-gToNamedJson = Object . HM.fromList . gToTupleList [] . from
+gToNamedJson a = Object . HM.fromList $ evalState (gToTupleList $ from a) []
 
 type UsedName = Text
 
 class GNamedToJSON a where
-    gToTupleList :: [UsedName] -> a x -> [(Text, Value)]
+    gToTupleList :: a x -> State [UsedName] [(Text, Value)]
 
 instance (GNamedToJSON f, Datatype d) => GNamedToJSON (M1 D d f) where
-    gToTupleList used = gToTupleList used . unM1
+    gToTupleList = gToTupleList . unM1
 
 instance (GNamedToJSON f, Constructor c) => GNamedToJSON (M1 C c f) where
-    gToTupleList used a =
-        [("tag", String . T.pack $ conName a)] <> gToTupleList used (unM1 a)
+    gToTupleList a = do
+        used <- get
+        put $ used <> ["tag"]
+        rest <- gToTupleList $ unM1 a
+        pure $ [("tag", String . T.pack $ conName a)] <> rest
 
 instance (JsonFieldName t) => GNamedToJSON (M1 S c (Rec0 t)) where
-    gToTupleList used a = [(actualFieldName used $ fieldName @t, toJSON . unK1 $ unM1 a)]
+    gToTupleList a = do
+        used <- get
+        let fName = fieldName @t
+        put $ used <> [fName]
+        pure [(actualFieldName used fName, toJSON . unK1 $ unM1 a)]
 
 instance GNamedToJSON U1 where
-    gToTupleList _ U1 = []
+    gToTupleList U1 = pure []
 
 instance (GNamedToJSON a, GNamedToJSON b) => GNamedToJSON (a :*: b) where
-    gToTupleList used (a :*: b) =
-        let p1 = gToTupleList used a
-            p2 = gToTupleList (fst <$> p1) b
-        in  p1 <> p2
+    gToTupleList (a :*: b) = do
+        p1 <- gToTupleList a
+        p2 <- gToTupleList b
+        pure $ p1 <> p2
 
 instance (GNamedToJSON a, GNamedToJSON b) => GNamedToJSON (a :+: b) where
-    gToTupleList used = \case
-        L1 a -> gToTupleList used a
-        R1 a -> gToTupleList used a
+    gToTupleList = \case
+        L1 a -> gToTupleList a
+        R1 a -> gToTupleList a
 
 
 actualFieldName :: [UsedName] -> Text -> Text
@@ -53,50 +70,48 @@ actualFieldName used fName = fName <> case length (filter (== fName) used) of
 
 -----------------------------------------------------------------------------
 
-lookupKey :: Text -> Value -> Parser Value
+lookupKey :: Text -> Value -> StateT [UsedName] Parser Value
 lookupKey k = \case
-    Object o -> maybe (fail $ "No key " <> show k) pure $ HM.lookup k o
-    _        -> fail $ "Expected " <> show k <> " to be an object."
+    Object o -> do
+        used <- get
+        put $ used <> [k]
+        maybe (fail $ "No key " <> show k) pure $ HM.lookup k o
+    _ -> fail $ "Expected " <> show k <> " to be an object."
 
 gParseNamedJson :: (GNamedFromJSON (Rep a), Generic a) => Value -> Parser a
-gParseNamedJson = fmap to . gNamedFromJSON []
+gParseNamedJson v = to <$> evalStateT (gNamedFromJSON v) []
 
 class GNamedFromJSON a where
-  gNamedFromJSON :: [UsedName] -> Value -> Parser (a x)
+  gNamedFromJSON :: Value -> StateT [UsedName] Parser (a x)
 
 instance GNamedFromJSON p => GNamedFromJSON (M1 D f p) where
-    gNamedFromJSON used v = M1 <$> gNamedFromJSON used v
+    gNamedFromJSON v = M1 <$> gNamedFromJSON v
 
 instance (Constructor f, GNamedFromJSON p) => GNamedFromJSON (M1 C f p) where
-    gNamedFromJSON used v = do
-        c   <- M1 <$> gNamedFromJSON used v
+    gNamedFromJSON v = do
+        c   <- M1 <$> gNamedFromJSON v
         tag <- lookupKey "tag" v
         case tag of
             String t | T.unpack t == conName c -> pure c
             _ -> fail "Unknown tag"
 
 instance GNamedFromJSON U1 where
-    gNamedFromJSON _ _ = pure U1
+    gNamedFromJSON _ = pure U1
 
 instance (GNamedFromJSON a, GNamedFromJSON b) => GNamedFromJSON (a :+: b) where
-    gNamedFromJSON used vals =
-        L1 <$> gNamedFromJSON used vals <|> R1 <$> gNamedFromJSON used vals
+    gNamedFromJSON vals = L1 <$> gNamedFromJSON vals <|> R1 <$> gNamedFromJSON vals
 
 
 instance (GNamedFromJSON a, GNamedFromJSON b) => GNamedFromJSON (a :*: b) where
-    gNamedFromJSON used vals =
-        (:*:) <$> gNamedFromJSON [] vals <*> gNamedFromJSON [] vals
+    gNamedFromJSON vals = do
+        p1 <- gNamedFromJSON vals
+        p2 <- gNamedFromJSON vals
+        pure $ p1 :*: p2
 
 instance JsonFieldName t => GNamedFromJSON (M1 S c (Rec0 t)) where
-    gNamedFromJSON used vals = do
-        v <- lookupKey (actualFieldName used $ fieldName @t) vals
-        M1 . K1 <$> (parseJSON @t) v
-
-------------------------------------------------------------------------------
-
-
-data Duplicated = Duplicated Int Int
-    deriving (Show, Generic)
-
-instance ToJSON Duplicated where
-    toJSON = gToNamedJson
+    gNamedFromJSON vals = do
+        used <- get
+        let fName = fieldName @t
+        v <- lookupKey (actualFieldName used fName) vals
+        put $ used <> [fName]
+        lift $ M1 . K1 <$> (parseJSON @t) v
--- test/DomainDriven/Internal/WithNamedFieldSpec.hs
+++ test/DomainDriven/Internal/WithNamedFieldSpec.hs
@@ -1,58 +1,63 @@
+{-# LANGUAGE DerivingVia #-}
 module DomainDriven.Internal.WithNamedFieldSpec where
 
-import Prelude
+import           Prelude
 import           GHC.Generics
 import           Data.Aeson
 import           Data.Text                      ( Text )
-import DomainDriven.Internal.WithNamedField
+import           DomainDriven.Internal.WithNamedField
 import           DomainDriven.Internal.JsonFieldName
-import Test.QuickCheck
-import Test.QuickCheck.Arbitrary.ADT
-import Test.QuickCheck.Classes
-import Data.Proxy
-import Control.Monad
-import Test.Hspec
-import Data.String
-import Test.Hspec.QuickCheck
-import Data.Swagger
+import           Test.QuickCheck
+import           Test.QuickCheck.Arbitrary.ADT
+import           Test.QuickCheck.Classes
+import           Data.Proxy
+import           Control.Monad
+import           Test.Hspec
+import           Data.String
+import           Test.Hspec.QuickCheck
+import           Data.Swagger
 
 data Apa = Apan
     { namedField1 :: Int
     , namedField2 :: Double
     }
     deriving (Show, Eq, Ord, Generic)
+    deriving (FromJSON, ToJSON) via (WithNamedField Apa)
 
 instance Arbitrary Apa where
-  arbitrary = genericArbitrary
-instance FromJSON Apa where
-  parseJSON = gParseNamedJson
-
-instance ToJSON Apa where
-  toJSON = gToNamedJson
+    arbitrary = genericArbitrary
 
 newtype MyText = MyText Text
     deriving (Show, Eq, Ord, Generic)
     deriving newtype (FromJSON, ToJSON, IsString, ToSchema)
     deriving anyclass (JsonFieldName)
 
+------------------------------------------------------------------------------
+
+
+instance Arbitrary Duplicated where
+    arbitrary = genericArbitrary
+
 instance Arbitrary MyText where
-  arbitrary = elements ["hej", "hopp","kalle","anka","ekorre"]
+    arbitrary = elements ["hej", "hopp", "kalle", "anka", "ekorre"]
 
 data Bulle = Middag
     | Beer Int MyText
     | Kaffe MyText
     deriving (Show, Eq, Ord, Generic)
+    deriving (FromJSON, ToJSON) via (WithNamedField Bulle)
 
 instance Arbitrary Bulle where
-  arbitrary = genericArbitrary
-instance FromJSON Bulle where
-  parseJSON = gParseNamedJson
+    arbitrary = genericArbitrary
 
-instance ToJSON Bulle where
-  toJSON = gToNamedJson
+data Duplicated
+    = Duplicated1 Int Int Int String Int String
+    | Duplicated2 Int String Int Double
+    deriving (Show, Eq, Generic)
+    deriving (FromJSON, ToJSON) via (WithNamedField Duplicated)
 
 spec :: Spec
 spec = describe "Json tests" $ do
     void $ traverse (uncurry prop) (lawsProperties $ jsonLaws $ Proxy @Apa)
     void $ traverse (uncurry prop) (lawsProperties $ jsonLaws $ Proxy @Bulle)
-    xit "something something when there are duplicate field names..." False
+    void $ traverse (uncurry prop) (lawsProperties $ jsonLaws $ Proxy @Duplicated)
