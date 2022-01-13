9c1e3d49f300e793851a516c45f6442d1f16fdc3
Tommy Engström <tommy@succinct.se>
2022-01-13 09:57:55 +0100

    Add failing tests showing deadlock bug

--- test/DomainDriven/Persistance/PostgresIORefStateSpec.hs
+++ test/DomainDriven/Persistance/PostgresIORefStateSpec.hs
@@ -4,7 +4,9 @@ module DomainDriven.Persistance.PostgresIORefStateSpec where
 
 import           Control.Concurrent.Async
 import           Control.Monad
-import           Data.Aeson                     ( Value )
+import           Data.Aeson                     ( Value
+                                                , encode
+                                                )
 import qualified Data.Map                                     as M
 import           Data.String                    ( fromString )
 import qualified Data.Text                                    as T
@@ -206,3 +208,20 @@ storeModelSpec = describe "Test basic functionality" $ do
         c1 <- runAction p Store.handleStoreAction Store.ListItems
         (length c1, length c0)
             `shouldSatisfy` (\(after', before') -> after' - before' == n)
+    it "Running a command where there are unevaliated events" $ \p -> do
+        conn <- getConnection p
+        key  <- mkId
+        let iKey = Store.ItemKey nil
+        now <- getCurrentTime
+        _   <- execute
+            conn
+            (  "insert into \""
+            <> fromString (eventTableName p)
+            <> "\" (id, timestamp, event) \
+                \values (?, ?, ?)"
+            )
+            ( key
+            , now
+            , encode $ Store.AddedItem iKey (Store.ItemName "test item") (Store.Price 10)
+            )
+        runAction p Store.handleStoreAction $ Store.BuyItem iKey (Store.Quantity 1)
