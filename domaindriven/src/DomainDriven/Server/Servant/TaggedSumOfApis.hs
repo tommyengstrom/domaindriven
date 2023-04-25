6b8f861093459ccf233a44b95ee565ecf92e965d
Frederik Ramcke <frederik.ramcke@eirforsakring.se>
2023-04-25 11:01:26 +0200

    Approach 1: Wrap

--- domaindriven/src/DomainDriven/Server/Servant/TaggedSumOfApis.hs
+++ domaindriven/src/DomainDriven/Server/Servant/TaggedSumOfApis.hs
@@ -15,6 +15,7 @@ import GHC.TypeLits
 import Generics.SOP
     ( All
     , Generic (..)
+    , HasDatatypeInfo (..)
     , I (..)
     , NP (..)
     , NS (..)
@@ -56,20 +57,20 @@ instance
 
 class RecordOfServersFields (mkApiRecord :: Type -> Type) (m :: Type -> Type) where
     recordOfServersFromFields
-        :: NP I (ServerTs (GenericRecordFields (mkApiRecord AsApi)) m)
-        -> mkApiRecord (AsServerT m)
+        :: NP I (ServerTs (GenericRecordFields (Wrap (mkApiRecord AsApi))) m)
+        -> Wrap (mkApiRecord (AsServerT m))
     recordOfServersToFields
-        :: mkApiRecord (AsServerT m)
-        -> NP I (ServerTs (GenericRecordFields (mkApiRecord AsApi)) m)
+        :: Wrap (mkApiRecord (AsServerT m))
+        -> NP I (ServerTs (GenericRecordFields (Wrap (mkApiRecord AsApi))) m)
 
 instance
-    ( Generic (mkApiRecord (AsServerT m))
-    , Code (mkApiRecord (AsServerT m))
-        ~ '[ServerTs (GenericRecordFields (mkApiRecord AsApi)) m]
+    ( Generic (Wrap (mkApiRecord (AsServerT m)))
+    , Code (Wrap (mkApiRecord (AsServerT m)))
+        ~ '[ServerTs (GenericRecordFields (Wrap (mkApiRecord AsApi))) m]
     )
     => RecordOfServersFields mkApiRecord m
     where
-    recordOfServersFromFields = to @(mkApiRecord (AsServerT m)) . SOP . Z
+    recordOfServersFromFields = to . SOP . Z
     recordOfServersToFields x = case unSOP $ from x of
         Z servers -> servers
         S y -> case y of {}
@@ -105,7 +106,7 @@ instance
     => TaggedSumOfApisHasServers
         mkApiRecord
         (api ': apis)
-        ( 'FieldInfo label ': infos)
+        ('FieldInfo label ': infos)
         context
     where
     type ServerTs (api ': apis) m = ServerT api m ': ServerTs apis m
@@ -128,8 +129,8 @@ instance
 instance
     ( TaggedSumOfApisHasServers
         mkApiRecord
-        (GenericRecordFields (mkApiRecord AsApi))
-        (GenericRecordFieldInfos (mkApiRecord AsApi))
+        (GenericRecordFields (Wrap (mkApiRecord AsApi)))
+        (GenericRecordFieldInfos (Wrap (mkApiRecord AsApi)))
         context
     , forall m. RecordOfServersFields mkApiRecord m
     )
@@ -141,20 +142,22 @@ instance
 
     route _ context delayedServer =
         taggedSumOfRoutes @mkApiRecord
-            @(GenericRecordFields (mkApiRecord AsApi))
-            @(GenericRecordFieldInfos (mkApiRecord AsApi))
+            @(GenericRecordFields (Wrap (mkApiRecord AsApi)))
+            @(GenericRecordFieldInfos (Wrap (mkApiRecord AsApi)))
             context
-            (recordOfServersToFields . unRecordOfServers <$> delayedServer)
+            (recordOfServersToFields . Wrap . unRecordOfServers <$> delayedServer)
 
     hoistServerWithContext _ _ nt servers =
         RecordOfServers
+            . unWrap
             . recordOfServersFromFields
             . hoistTaggedServersWithContext @mkApiRecord
-                @(GenericRecordFields (mkApiRecord AsApi))
-                @(GenericRecordFieldInfos (mkApiRecord AsApi))
+                @(GenericRecordFields (Wrap (mkApiRecord AsApi)))
+                @(GenericRecordFieldInfos (Wrap (mkApiRecord AsApi)))
                 @context
                 nt
             . recordOfServersToFields
+            . Wrap
             $ unRecordOfServers servers
 
 class
@@ -174,7 +177,7 @@ instance
     , HasOpenApi api
     , TaggedSumOfApisHasOpenApi mkApiRecord apis infos
     )
-    => TaggedSumOfApisHasOpenApi mkApiRecord (api ': apis) ( 'FieldInfo label ': infos)
+    => TaggedSumOfApisHasOpenApi mkApiRecord (api ': apis) ('FieldInfo label ': infos)
     where
     taggedSumOfApisToOpenApi =
         prependPath
@@ -201,3 +204,18 @@ instance
     => ThrowAll (RecordOfServers a)
     where
     throwAll = to . throwAll @(SOP I (Code (RecordOfServers a)))
+
+newtype Wrap a = Wrap { unWrap :: a }
+    deriving newtype (GHC.Generic)
+
+instance (GFrom a, GHC.Generic a, GTo a, (All SListI (GCode a))) => Generic (Wrap a) where
+    type Code (Wrap a) = GCode a
+    from (Wrap a) = gfrom a
+    to = Wrap . gto
+
+instance
+    (Generic (Wrap a), GDatatypeInfo (Wrap a))
+    => HasDatatypeInfo (Wrap a)
+    where
+    type DatatypeInfoOf (Wrap a) = GDatatypeInfoOf a
+    datatypeInfo = gdatatypeInfo
