eace4abb066d0504d00bb0fd37408d40b6093b50
Tommy Engström <tommy@succinct.se>
2023-05-15 16:30:49 +0200

    Show what is not a record

--- domaindriven/src/DomainDriven/Server/Helper/GenericRecord.hs
+++ domaindriven/src/DomainDriven/Server/Helper/GenericRecord.hs
@@ -12,11 +12,11 @@ type family GenericRecordFields (record :: Type) :: [Type] where
 
 type family GenericRecordFields' (code :: [[Type]]) :: [Type] where
     GenericRecordFields' '[fields] = fields
-    GenericRecordFields' _ = TypeError ('Text "not a record!")
+    GenericRecordFields' t = TypeError ('ShowType t ':<>: 'Text " is not a record!")
 
 type family GenericRecordFieldInfos (record :: Type) :: [FieldInfo] where
     GenericRecordFieldInfos record = GenericRecordFieldInfos' (GDatatypeInfoOf record)
 
 type family GenericRecordFieldInfos' (info :: DatatypeInfo) :: [FieldInfo] where
     GenericRecordFieldInfos' ('ADT _ _ '[ 'Record _ infos] _) = infos
-    GenericRecordFieldInfos' _ = TypeError ('Text "not a record!")
+    GenericRecordFieldInfos' t = TypeError ('ShowType t ':<>: 'Text " is not a record!")
